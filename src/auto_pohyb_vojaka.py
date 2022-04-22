42d29fbb8c85620ecdbad4a981dc911cbca8d663
Ondřej Frejvald <ondrej.frejvald@gmail.com>
2022-04-22 11:36:58 +0200

    generování vojáků (chybové, není ještě hotovo)

--- src/auto_pohyb_vojaka.py
+++ src/auto_pohyb_vojaka.py
@@ -14,7 +14,17 @@ bod1_x = random.randint(0,ROZLISENI_X)
 bod2_y = random.randint(0,ROZLISENI_Y)
 bod2_x = random.randint(0, ROZLISENI_X)
 
+#Bod 3
+
 a = math.atan2(bod2_y - bod1_y,bod2_x - bod1_x)
+
+
+k = 100
+seznam_vojaku = []
+
+for nabor in range(k):
+    vojak = (random.randint(bod1_x, bod2_x), random.randint(bod1_y, bod2_y))
+    seznam_vojaku.append(vojak)
 
 pygame.init()
 
@@ -37,11 +47,11 @@ while True:
     if x > bod2_x and bod2_x < bod1_x:
         x += 0.5 * math.cos(a)
         y += 0.5 * math.sin(a)
-        
+            
     okno.fill(BARVA_POZADI)
     
-    pygame.draw.circle(okno,(255,0,0),(x,y),5)
-    
-    
+    pygame.draw.circle(okno,(255,8,0),(x,y),5)
+    for v in seznam_vojaku:
+        pygame.draw.circle(okno,(255,8,0),v,5)
     
     pygame.display.update()
