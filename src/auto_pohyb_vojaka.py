955016f302f277c220bf1a377d0e9d4a05df940f
Kettner Samuel <kettners21@spstrutnov.cz>
2022-04-29 13:21:13 +0200

    sice to nefunguje, ale je tu základ pro pohyb

--- src/auto_pohyb_vojaka.py
+++ src/auto_pohyb_vojaka.py
@@ -7,24 +7,24 @@ ROZLISENI_OKNA = ROZLISENI_X, ROZLISENI_Y = 1800,900
 BARVA_POZADI = 255,255,255
 cas = pygame.time.Clock()
 bod_v_case = pygame.time.get_ticks()
+bod2_v_case = pygame.time.get_ticks()
+rad = 5
 
+#Bod 1 bod spawnu míčku, který leze
+bod1_y = ROZLISENI_Y - ROZLISENI_Y/3
+bod1_x = 300
 
+#Bod 2, bod 
+bod2_y = 200
+bod2_x = ROZLISENI_X - 300
 
-#Bod 1
-bod1_y = random.randint(0,ROZLISENI_Y)
-bod1_x = random.randint(0,ROZLISENI_X)
+#bod 3, bod začáku základny
+bod3_x = rad
+bod3_y = ROZLISENI_Y - ROZLISENI_Y/3
 
-#Bod 2
-bod2_y = random.randint(0,ROZLISENI_Y)
-bod2_x = random.randint(0, ROZLISENI_X)
-
-#bod 3
-bod3_x = 100
-bod3_y = 100
-
-#bod 4
-bod4_x = ROZLISENI_X - 100
-bod4_y = ROZLISENI_Y -100
+#bod 4, bod konce základny 
+bod4_x = bod1_x
+bod4_y = bod1_y - rad
 
 bod3_x, bod4_x = min(bod3_x, bod4_x), max(bod3_x, bod4_x)
 bod3_y, bod4_y = min(bod3_y, bod4_y), max(bod3_y, bod4_y)
@@ -34,8 +34,9 @@ bod3_y, bod4_y = min(bod3_y, bod4_y), max(bod3_y, bod4_y)
 a = math.atan2(bod2_y - bod1_y,bod2_x - bod1_x)
 
 
-pocatecni_pocet_vojaku = 100
+pocatecni_pocet_vojaku = 1
 seznam_vojaku = []
+seznam_vojaku_na_pochodu = []
 
 for nabor in range(pocatecni_pocet_vojaku):
     vojak = (random.randint(bod3_x, bod4_x), random.randint(bod3_y, bod4_y))
@@ -55,16 +56,21 @@ while True:
             sys.exit()
     cas_ted = pygame.time.get_ticks()
     od_minula_ms = cas_ted - bod_v_case
-    
-    if od_minula_ms > 1000:
+    od_minula2_ms = cas_ted - bod2_v_case
+    if od_minula_ms > 700:
         vojak = (random.randint(bod3_x, bod4_x), random.randint(bod3_y, bod4_y))
         seznam_vojaku.append(vojak)
-        
-        bod_v_case = cas_ted 
+        bod_v_case = cas_ted
+    if od_minula2_ms > 500:
+        vojak_pochodujici = (x,y)
+        seznam_vojaku_na_pochodu.append(vojak_pochodujici)
+        bod2_v_case = cas_ted
     okno.fill(BARVA_POZADI)
     
     # pozůstatek jezdícího míčku: pygame.draw.circle(okno,(255,8,0),(x,y),5)
     for v in seznam_vojaku:
-        pygame.draw.circle(okno,(255,8,0),v,5)
-    
+        pygame.draw.circle(okno,(255,8,0),v,rad)
+    for p_v in seznam_vojaku_na_pochodu:
+        pygame.draw.circle(okno,(255,8,0),(p_v),rad)
+    x += 0.1    
     pygame.display.update()
