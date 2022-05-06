d870ed78af8fa0fdd49fc8bd25ee09f3e37de4df
Pavel Kotek <kotekp21@spstrutnov.cz>
2022-05-06 10:47:35 +0200

    Přepínání mezi dvěma seznami šipkou nahoru

--- src/přepínání mezi objekty.py
+++ src/přepínání mezi objekty.py
@@ -7,6 +7,7 @@ ROZLISENI_OKNA = ROZLISENI_X, ROZLISENI_Y = 800, 600
 BARVA_POZADI = 0, 0, 0
 BARVA_OBJEKTU = 200, 30, 30
 BARVA_OBJEKTU_ZVYRAZNENO = 255, 255, 255
+BARVA_DRUHA = 0,255,0
 w = 50
 h = 50
 w2 = 70
@@ -31,6 +32,16 @@ seznam_objektu = [
                     {'pozice_x':550, 'pozice_y': 400, 'vybrano': False},
                     ]
 
+sekundarni_seznam =[
+                    {'pozice_x':150, 'pozice_y': 200, 'vybrano': True},
+                    {'pozice_x':250, 'pozice_y': 200, 'vybrano': False},
+                    {'pozice_x':350, 'pozice_y': 200, 'vybrano': False},
+                    {'pozice_x':450, 'pozice_y': 200, 'vybrano': False},
+                    {'pozice_x':550, 'pozice_y': 200, 'vybrano': False},
+                   ]
+
+aktivni = seznam_objektu
+povoleni_a = True
 
 while True:
     udalosti = pygame.event.get()
@@ -65,23 +76,33 @@ while True:
     if k_left == True:
         if stisknuto[pygame.K_LEFT] == False:
             k_left = False
-    
+            
+    # Přepínání mezi seznamy
+    if stisknuto[pygame.K_UP] or stisknuto[pygame.K_DOWN]:
+        if povoleni_a == True:
+            povoleni_a = False
+            if aktivni == sekundarni_seznam:
+                aktivni = seznam_objektu
+            elif aktivni == seznam_objektu:
+                aktivni = sekundarni_seznam
+    else:
+        povoleni_a = True
     
     # vybrani dalsiho objektu pri stisku klavesy RIGHT
     if povoleni_r == True:
-        for i, objekt in enumerate(seznam_objektu):
-            if seznam_objektu[i]['vybrano'] == True:
-                seznam_objektu[(i + 1) % len(seznam_objektu)]['vybrano'] = True
-                seznam_objektu[i]['vybrano'] = False
+        for i, objekt in enumerate(aktivni):
+            if aktivni[i]['vybrano'] == True:
+                aktivni[(i + 1) % len(aktivni)]['vybrano'] = True
+                aktivni[i]['vybrano'] = False
                 break
         povoleni_r = False
                     
     # vybrani dalsiho objektu pri stisku klavesy LEFT
     if povoleni_l == True:
-        for i, objekt in enumerate(seznam_objektu):
-            if seznam_objektu[i]['vybrano'] == True:
-                seznam_objektu[(i - 1) % len(seznam_objektu)]['vybrano'] = True
-                seznam_objektu[i]['vybrano'] = False
+        for i, objekt in enumerate(aktivni):
+            if aktivni[i]['vybrano'] == True:
+                aktivni[(i - 1) % len(aktivni)]['vybrano'] = True
+                aktivni[i]['vybrano'] = False
                 break
         povoleni_l = False
         
@@ -90,12 +111,21 @@ while True:
            
     for objekt in seznam_objektu:
         
-        if objekt['vybrano'] == True:
-            pygame.draw.rect(okno, (BARVA_OBJEKTU_ZVYRAZNENO), (objekt['pozice_x'] - rozdil_ctverecku, objekt['pozice_y'] - rozdil_ctverecku, w2, h2))
-            pygame.draw.rect(okno, (BARVA_OBJEKTU), (objekt['pozice_x'], objekt['pozice_y'], w, h))
+        if aktivni == seznam_objektu:
+            if objekt['vybrano'] == True:
+                pygame.draw.rect(okno, (BARVA_OBJEKTU_ZVYRAZNENO), (objekt['pozice_x'] - rozdil_ctverecku, objekt['pozice_y'] - rozdil_ctverecku, w2, h2))
+                pygame.draw.rect(okno, (BARVA_OBJEKTU), (objekt['pozice_x'], objekt['pozice_y'], w, h))
             
         pygame.draw.rect(okno, (BARVA_OBJEKTU), (objekt['pozice_x'], objekt['pozice_y'], w, h))
 
+    for objekt in sekundarni_seznam:
+        
+        if aktivni == sekundarni_seznam:
+            if objekt['vybrano'] == True:
+                pygame.draw.rect(okno, (BARVA_OBJEKTU_ZVYRAZNENO), (objekt['pozice_x'] - rozdil_ctverecku, objekt['pozice_y'] - rozdil_ctverecku, w2, h2))
+                pygame.draw.rect(okno, (BARVA_DRUHA), (objekt['pozice_x'], objekt['pozice_y'], w, h))
+            
+        pygame.draw.rect(okno, (BARVA_DRUHA), (objekt['pozice_x'], objekt['pozice_y'], w, h))
     
 
     pygame.display.update()
