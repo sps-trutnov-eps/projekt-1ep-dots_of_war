ac2753998cc8cbd2d3d85b713979dca9b59e2424
Ondřej Frejvald <ondrej.frejvald@gmail.com>
2022-05-06 11:30:27 +0200

    celkem zmatečný pohyb - Kettner, Frejvald

--- src/auto_pohyb_vojaka.py
+++ src/auto_pohyb_vojaka.py
@@ -24,22 +24,28 @@ bod3_y = ROZLISENI_Y - ROZLISENI_Y/3
 
 #bod 4, bod konce základny 
 bod4_x = bod1_x
-bod4_y = bod1_y - rad
+bod4_y = ROZLISENI_Y - rad
 
 bod3_x, bod4_x = min(bod3_x, bod4_x), max(bod3_x, bod4_x)
 bod3_y, bod4_y = min(bod3_y, bod4_y), max(bod3_y, bod4_y)
+ 
+#spawn vojáků
+vojak_x = random.randint(0,300)
+vojak_y = random.randint(ROZLISENI_Y - 300,ROZLISENI_Y)
 
-
+#pohyb
+hledani_x = -1
 
 a = math.atan2(bod2_y - bod1_y,bod2_x - bod1_x)
 
-
+v_vojaka = 40
 pocatecni_pocet_vojaku = 1
 seznam_vojaku = []
 seznam_vojaku_na_pochodu = []
+x_pohybujici = []
 
 for nabor in range(pocatecni_pocet_vojaku):
-    vojak = (random.randint(bod3_x, bod4_x), random.randint(bod3_y, bod4_y))
+    vojak = [vojak_x,vojak_y]
     seznam_vojaku.append(vojak)
 
 pygame.init()
@@ -54,17 +60,19 @@ while True:
         if u.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    stisknuto = pygame.key.get_pressed()
+    vojak_x = random.randint(0,300)
+    vojak_y = random.randint(ROZLISENI_Y - 300,ROZLISENI_Y)       
     cas_ted = pygame.time.get_ticks()
     od_minula_ms = cas_ted - bod_v_case
     od_minula2_ms = cas_ted - bod2_v_case
     if od_minula_ms > 700:
-        vojak = (random.randint(bod3_x, bod4_x), random.randint(bod3_y, bod4_y))
+        vojak = [vojak_x, vojak_y]
         seznam_vojaku.append(vojak)
+        for vojak in seznam_vojaku:
+            vojak[0] += 400
         bod_v_case = cas_ted
-    if od_minula2_ms > 500:
-        vojak_pochodujici = (x,y)
-        seznam_vojaku_na_pochodu.append(vojak_pochodujici)
-        bod2_v_case = cas_ted
+        
     okno.fill(BARVA_POZADI)
     
     # pozůstatek jezdícího míčku: pygame.draw.circle(okno,(255,8,0),(x,y),5)
@@ -72,5 +80,5 @@ while True:
         pygame.draw.circle(okno,(255,8,0),v,rad)
     for p_v in seznam_vojaku_na_pochodu:
         pygame.draw.circle(okno,(255,8,0),(p_v),rad)
-    x += 0.1    
+        
     pygame.display.update()
