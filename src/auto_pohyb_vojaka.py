d8d18f57a22c0e4525a3076e37f52b04d544c6ef
Ondřej Klápště <klapsteo21@spstrutnov.cz>
2022-05-11 09:36:59 +0200

    Cara s mickem

--- src/auto_pohyb_vojaka.py
+++ src/auto_pohyb_vojaka.py
@@ -5,23 +5,24 @@ import math
 
 ROZLISENI_OKNA = ROZLISENI_X, ROZLISENI_Y = 1800,900
 BARVA_POZADI = 255,255,255
+#cara
+zacatek_cary = 100, ROZLISENI_Y/2
+konec_cary = ROZLISENI_X - 100, ROZLISENI_Y/2
+tloustka_cary = 3
+
+#vojak
 
 #Bod 1
-bod1_y = random.randint(0,ROZLISENI_Y)
-bod1_x = random.randint(0,ROZLISENI_X)
+bod_xy = zacatek_cary
 
 #Bod 2
-bod2_y = random.randint(0,ROZLISENI_Y)
-bod2_x = random.randint(0, ROZLISENI_X)
-
-a = math.atan2(bod2_y - bod1_y,bod2_x - bod1_x)
+bod_xy2 = konec_cary
 
 pygame.init()
 
-pygame.display.set_caption('pozor chodec')
+pygame.display.set_caption('Střet vojáků')
 okno = pygame.display.set_mode(ROZLISENI_OKNA)
-x = bod1_x
-y = bod1_y
+
 while True:
     udalosti = pygame.event.get()
     for u in udalosti:
@@ -31,16 +32,12 @@ while True:
         if u.type == pygame.MOUSEWHEEL:
             if u.y < 0:
                 pygame.display.iconify()
-    if x < bod2_x and bod2_x > bod1_x:
-        x += 0.5 * math.cos(a)
-        y += 0.5 * math.sin(a)
-    if x > bod2_x and bod2_x < bod1_x:
-        x += 0.5 * math.cos(a)
-        y += 0.5 * math.sin(a)
+              
         
     okno.fill(BARVA_POZADI)
     
-    pygame.draw.circle(okno,(255,0,0),(x,y),5)
+    pygame.draw.line(okno, (0,0,0), (zacatek_cary), (konec_cary), (tloustka_cary))
+    pygame.draw.ellipse(okno,(255,0,0),(bod_xy),5)
     
     
     
