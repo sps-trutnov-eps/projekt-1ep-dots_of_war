414d97987fe8a3b8b3edad9454ab99c9e107a106
Kačer Šimon <kacers21@spstrutnov.cz>
2022-05-11 08:46:38 +0200

    Vojáci už odcházejí na frontu, ale musí se udělat ještě jeden mezičas

--- src/auto_pohyb_vojaka.py
+++ src/auto_pohyb_vojaka.py
@@ -3,10 +3,12 @@ import sys
 import random
 import math
 
+####### Parametry #######
 ROZLISENI_OKNA = ROZLISENI_X, ROZLISENI_Y = 1800,900
 BARVA_POZADI = 255,255,255
 cas = pygame.time.Clock()
-bod_v_case = pygame.time.get_ticks()
+cas_spawnu_posledniho_vojaka = pygame.time.get_ticks()
+cas_odchodu_posledniho_vojaka_na_frontu = pygame.time.get_ticks()
 souradnice_kasaren_x = 0
 souradnice_kasaren_y = 650
 souradnice_bileho_obdelniku_x = 10
@@ -18,21 +20,15 @@ velikost_kasaren_y = 250
 startovni_pozice_cary_XY = 250, 775
 konecna_pozice_cary_XY = 850, 775 
 
-#Bod 1
+####### Souřadnice kasáren levého horního bodu #######
 vojaci_kasarny_levy_horni_bod_y = souradnice_bileho_obdelniku_y
 vojaci_kasarny_levy_horni_bod_x = souradnice_bileho_obdelniku_x
 
-#Bod 2
+####### Souřadnice kasáren pravého dolního bodu #######
 vojaci_kasarny_pravy_dolni_bod_y = souradnice_bileho_obdelniku_y + velikost_kasaren_y
 vojaci_kasarny_pravy_dolni_bod_x = souradnice_bileho_obdelniku_x + velikost_kasaren_x
 
-
-
-#Bod 3
-
-a = math.atan2(vojaci_kasarny_pravy_dolni_bod_y - vojaci_kasarny_levy_horni_bod_y,vojaci_kasarny_pravy_dolni_bod_x - vojaci_kasarny_levy_horni_bod_x)
-
-
+####### Nábor vojáků #######
 pocatecni_pocet_vojaku = 50 
 seznam_vojaku = []
 seznam_vojaku_na_ceste = []
@@ -48,8 +44,6 @@ pygame.init()
 
 pygame.display.set_caption('pozor chodec')
 okno = pygame.display.set_mode(ROZLISENI_OKNA)
-x = vojaci_kasarny_levy_horni_bod_x
-y = vojaci_kasarny_levy_horni_bod_y
 dvere_otevreny = False
 while True:
     udalosti = pygame.event.get()
@@ -59,31 +53,40 @@ while True:
             sys.exit()
         if u.type == pygame.KEYDOWN and u.key == pygame.K_SPACE:
             dvere_otevreny = not dvere_otevreny
+        if u.type == pygame.KEYDOWN and u.key == pygame.K_ESCAPE:
+            pygame.quit()
+            sys.exit()
             
-    cas_ted = pygame.time.get_ticks()
-    od_minula_ms = cas_ted - bod_v_case
+            
+        ####### Spawnování vojáků #######  
+    doba_od_uplynuti_spawnu_vojaka  = pygame.time.get_ticks() - cas_spawnu_posledniho_vojaka
     
-    if od_minula_ms > 1000:  
+    if doba_od_uplynuti_spawnu_vojaka > 1000:
         vojak = [
              random.randint(vojaci_kasarny_levy_horni_bod_x, vojaci_kasarny_pravy_dolni_bod_x-26),
              random.randint(vojaci_kasarny_levy_horni_bod_y, vojaci_kasarny_pravy_dolni_bod_y-26),
             ]
         seznam_vojaku.append(vojak)
         
-        bod_v_case = cas_ted
+        cas_spawnu_posledniho_vojaka = pygame.time.get_ticks()
         
-    if dvere_otevreny and seznam_vojaku:
-        vojak_nove_na_ceste = seznam_vojaku.pop()
-        seznam_vojaku_na_ceste.append(vojak_nove_na_ceste)
-        vojak_nove_na_ceste[0], vojak_nove_na_ceste[1] = startovni_pozice_cary_XY
-    for v in seznam_vojaku_na_ceste:
-        v[0] = v[0] + 1
+         ####### Otevření dveří #######
+    doba_od_odchodu_posledniho_vojaka_na_frontu = pygame.time.get_ticks() - cas_odchodu_posledniho_vojaka_na_frontu
+    
+    if doba_od_odchodu_posledniho_vojaka_na_frontu > 500:
+        if dvere_otevreny and seznam_vojaku:
+            vojak_nove_na_ceste = seznam_vojaku.pop()
+            seznam_vojaku_na_ceste.append(vojak_nove_na_ceste)
+            vojak_nove_na_ceste[0], vojak_nove_na_ceste[1] = startovni_pozice_cary_XY
+            cas_odchodu_posledniho_vojaka_na_frontu = pygame.time.get_ticks()        
             
+        for v in seznam_vojaku_na_ceste:
+            v[0] += 1
             
             
    
     
-            
+          ####### Vykreslování tvarů #######   
     okno.fill(BARVA_POZADI)
     
     pygame.draw.rect(okno, (0,0,0), ((souradnice_kasaren_x, souradnice_kasaren_y), (velikost_kasaren_x, velikost_kasaren_y)))
