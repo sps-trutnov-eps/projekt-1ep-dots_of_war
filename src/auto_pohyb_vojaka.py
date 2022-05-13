613799a54a689c09753a8b81736b15fe6f634e22
Ondřej Klápště <klapsteo21@spstrutnov.cz>
2022-05-13 12:13:32 +0200

    Počítání úhlu

--- src/auto_pohyb_vojaka.py
+++ src/auto_pohyb_vojaka.py
@@ -8,15 +8,36 @@ BARVA_POZADI = 255,255,255
 #cara
 zacatek_cary = 100, ROZLISENI_Y/2
 konec_cary = ROZLISENI_X - 100, ROZLISENI_Y/2
-tloustka_cary = 3
 
-#vojak
+
+#cara rovne
+x_cary = 0 
+y_cary = ROZLISENI_Y - 300 
+x1_cary = 300 
+y1_cary = ROZLISENI_Y - 300
+tloustka_cary = 3
+#cara dolu
+x_cary1 = 300 
+y_cary1 = ROZLISENI_Y - 300 
+x1_cary1 = 300 
+y1_cary1 = ROZLISENI_Y
 
 #Bod 1
-bod_xy = zacatek_cary
+bod_x1 = 300
+bod_y1 = ROZLISENI_Y - 300
 
 #Bod 2
-bod_xy2 = konec_cary
+bod_x2 = 1800
+bod_y2 = 0
+
+#rychlosti
+d = 5
+#pohyb micku
+uhel_micku= math.atan2(bod_y2 - bod_y1, bod_x2 - bod_x1)
+dy = d * math.sin(uhel_micku)
+dx = d * math.cos(uhel_micku)
+
+
 
 pygame.init()
 
@@ -36,9 +57,10 @@ while True:
         
     okno.fill(BARVA_POZADI)
     
-    pygame.draw.line(okno, (0,0,0), (zacatek_cary), (konec_cary), (tloustka_cary))
-    pygame.draw.circle(okno,(255,0,0),(bod_xy), 5)
-    
+    pygame.draw.line(okno, (0,0,0), (300, ROZLISENI_Y - 300), (1800,0), (tloustka_cary))
+    pygame.draw.line(okno,(0,0,0),(x_cary,y_cary),(x1_cary,y1_cary),tloustka_cary) 
+    pygame.draw.line(okno,(0,0,0),(x_cary1,y_cary1),(x1_cary1,y1_cary1),tloustka_cary)
+    vojak = pygame.draw.circle(okno,(255,0,0),(bod_x1, bod_y1), 5)
     
     
     pygame.display.update()
