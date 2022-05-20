b4f8de896f7b622f66e35a4152dabd8f0af31c43
Ondřej Klápště <klapsteo21@spstrutnov.cz>
2022-05-20 11:35:37 +0200

    Vojaci umrou

--- src/auto_pohyb_vojaka.py
+++ src/auto_pohyb_vojaka.py
@@ -10,43 +10,49 @@ tloustka_cary = 3
 
 #cara rovne
 x1_cary_rovne = 0 
-y1_cary_rovne = ROZLISENI_Y - 300 
-x2_cary_rovne = 300 
-y2_cary_rovne = ROZLISENI_Y - 300
+y1_cary_rovne = ROZLISENI_Y - 100 
+x2_cary_rovne = 100 
+y2_cary_rovne = ROZLISENI_Y - 100
 
 #cara dolu
-x1_cary_dolu = 300 
-y1_cary_dolu = ROZLISENI_Y - 300 
-x2_cary_dolu = 300
+x1_cary_dolu = 100 
+y1_cary_dolu = ROZLISENI_Y - 100
+x2_cary_dolu = 100
 y2_cary_dolu = ROZLISENI_Y
 
 #vojak cerveny
-vojak_x = 300
-vojak_y = ROZLISENI_Y - 300
+vojak_x = 100
+vojak_y = ROZLISENI_Y - 100
 #vojak modry
-vojak_modry_x = 1800 - 5
-vojak_modry_y = 0 
+vojak_modry_x = 1600
+vojak_modry_y = 100 
 
 #sikma cara
-x1_cary_sikmo = 300
-y1_cary_sikmo = ROZLISENI_Y - 300
-x2_cary_sikmo = 1800
-y2_cary_sikmo = 0
+x1_cary_sikmo = 100
+y1_cary_sikmo = ROZLISENI_Y - 100
+x2_cary_sikmo = 1600
+y2_cary_sikmo = 100
 
 
 #rychlosti
-v = 0.1
+v = 0.2
 #pohyb červeného míčku
-uhel_micku_cerveneho= math.atan2(y2_cary_sikmo - y1_cary_sikmo, x2_cary_sikmo - x1_cary_sikmo)
+uhel_micku_cerveneho = math.atan2(y2_cary_sikmo - y1_cary_sikmo, x2_cary_sikmo - x1_cary_sikmo)
 dy = v * math.sin(uhel_micku_cerveneho)
 dx = v * math.cos(uhel_micku_cerveneho)
 pygame.init()
 #pohyb modrého míčku
+uhel_micku_modreho = math.atan2(x1_cary_sikmo - x2_cary_sikmo, y1_cary_sikmo - y2_cary_sikmo)
+dy1 = v * math.sin(uhel_micku_modreho)
+dx1 = v * math.cos(uhel_micku_modreho)
 
+dostrel = 15
 
 pygame.display.set_caption('Střet vojaku')
 okno = pygame.display.set_mode(ROZLISENI_OKNA)
 
+zije = True
+zije = pygame.draw.circle(okno,(255,0,0),(vojak_x, vojak_y), 10), pygame.draw.circle(okno,(0, 150, 255),(vojak_modry_x, vojak_modry_y ), 10)
 while True:
     udalosti = pygame.event.get()
     for u in udalosti:
@@ -60,10 +66,19 @@ while True:
             if u.y < 0:
                 pygame.display.iconify()
              
-        
+    #pohyb cerveny
     vojak_x += dx
     vojak_y += dy
-   
+
+    #pohyb modry
+    vojak_modry_x -= dx
+    vojak_modry_y -= dy
+    
+    #kolize s vojaky
+    c = ((vojak_x - vojak_modry_x)**2 - (vojak_modry_y - vojak_y)**2) ** (1/2)
+    if c < dostrel:
+        zije = False
+        
     
     
      
@@ -72,8 +87,10 @@ while True:
     pygame.draw.line(okno, (0,0,0), (x1_cary_sikmo, y1_cary_sikmo), (x2_cary_sikmo, y2_cary_sikmo), (tloustka_cary))
     pygame.draw.line(okno,(0,0,0),(x1_cary_rovne,y1_cary_rovne),(x2_cary_rovne,y2_cary_rovne),tloustka_cary) 
     pygame.draw.line(okno,(0,0,0),(x1_cary_dolu,y1_cary_dolu),(x2_cary_dolu,y2_cary_dolu),tloustka_cary)
-    pygame.draw.circle(okno,(255,0,0),(vojak_x, vojak_y), 10)
-    pygame.draw.circle(okno,(0, 150, 255),(vojak_modry_x, vojak_modry_y ), 10)
+    if zije:
+        pygame.draw.circle(okno,(255,0,0),(vojak_x, vojak_y), 10)
+    if zije:
+        pygame.draw.circle(okno,(0, 150, 255),(vojak_modry_x, vojak_modry_y ), 10)
     
     
     pygame.display.update()
