608f65fd0395e7c188ae43f0bad737c4f3ed2c09
Ondřej Klápště <klapsteo21@spstrutnov.cz>
2022-05-27 12:13:30 +0200

    Vojaci umrou, zastaví se, zobrazí se místo smrti

--- src/auto_pohyb_vojaka.py
+++ src/auto_pohyb_vojaka.py
@@ -7,6 +7,12 @@ ROZLISENI_OKNA = ROZLISENI_X, ROZLISENI_Y = 1800,900
 BARVA_POZADI = 255,255,255
 
 tloustka_cary = 3
+r = 10
+
+
+seda = (128, 128, 128)
+cervena = (255,0,0)
+modra = (0, 150, 255)
 
 #cara rovne
 x1_cary_rovne = 0 
@@ -35,7 +41,8 @@ y2_cary_sikmo = 100
 
 
 #rychlosti
-v = 0.2
+v = 0.3
+
 #pohyb červeného míčku
 uhel_micku_cerveneho = math.atan2(y2_cary_sikmo - y1_cary_sikmo, x2_cary_sikmo - x1_cary_sikmo)
 dy = v * math.sin(uhel_micku_cerveneho)
@@ -46,7 +53,7 @@ uhel_micku_modreho = math.atan2(x1_cary_sikmo - x2_cary_sikmo, y1_cary_sikmo - y
 dy1 = v * math.sin(uhel_micku_modreho)
 dx1 = v * math.cos(uhel_micku_modreho)
 
-dostrel = 15
+dostrel = 12
 
 pygame.display.set_caption('Střet vojaku')
 okno = pygame.display.set_mode(ROZLISENI_OKNA)
@@ -67,30 +74,30 @@ while True:
                 pygame.display.iconify()
              
     #pohyb cerveny
-    vojak_x += dx
-    vojak_y += dy
+    if zije:
+        vojak_x += dx
+        vojak_y += dy
 
     #pohyb modry
-    vojak_modry_x -= dx
-    vojak_modry_y -= dy
+    if zije:
+        vojak_modry_x -= dx
+        vojak_modry_y -= dy
     
     #kolize s vojaky
     c = ((vojak_x - vojak_modry_x)**2 - (vojak_modry_y - vojak_y)**2) ** (1/2)
     if c < dostrel:
         zije = False
+        modra = seda
+        cervena = seda
+        r = 8
         
-    
-    
-     
     okno.fill(BARVA_POZADI)
     
     pygame.draw.line(okno, (0,0,0), (x1_cary_sikmo, y1_cary_sikmo), (x2_cary_sikmo, y2_cary_sikmo), (tloustka_cary))
     pygame.draw.line(okno,(0,0,0),(x1_cary_rovne,y1_cary_rovne),(x2_cary_rovne,y2_cary_rovne),tloustka_cary) 
     pygame.draw.line(okno,(0,0,0),(x1_cary_dolu,y1_cary_dolu),(x2_cary_dolu,y2_cary_dolu),tloustka_cary)
-    if zije:
-        pygame.draw.circle(okno,(255,0,0),(vojak_x, vojak_y), 10)
-    if zije:
-        pygame.draw.circle(okno,(0, 150, 255),(vojak_modry_x, vojak_modry_y ), 10)
+    pygame.draw.circle(okno,cervena,(vojak_x, vojak_y), r)
+    pygame.draw.circle(okno,modra,(vojak_modry_x, vojak_modry_y ), r)
     
     
     pygame.display.update()
