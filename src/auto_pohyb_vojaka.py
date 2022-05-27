164c75dfd48a547657e1e29d7789d488b6af5d37
Ondřej Frejvald <ondrej.frejvald@gmail.com>
2022-05-27 13:20:58 +0200

    zacatek seznamu

--- src/auto_pohyb_vojaka.py
+++ src/auto_pohyb_vojaka.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import random
 import math
+pygame.init()
 
 ROZLISENI_OKNA = ROZLISENI_X, ROZLISENI_Y = 1800,900
 BARVA_POZADI = 255,255,255
@@ -26,13 +27,6 @@ y1_cary_dolu = ROZLISENI_Y - 100
 x2_cary_dolu = 100
 y2_cary_dolu = ROZLISENI_Y
 
-#vojak cerveny
-vojak_x = 100
-vojak_y = ROZLISENI_Y - 100
-#vojak modry
-vojak_modry_x = 1600
-vojak_modry_y = 100 
-
 #sikma cara
 x1_cary_sikmo = 100
 y1_cary_sikmo = ROZLISENI_Y - 100
@@ -43,15 +37,35 @@ y2_cary_sikmo = 100
 #rychlosti
 v = 0.3
 
+#spawn wojaka
+def spawn(color):
+    vojak = []
+    if color == "red":
+        vojak.append([100,ROZLISENI_Y - 100])
+    else:
+        vojak.append([1600,100])
+    vojak.append(color)
+    return(vojak)
+
+#seznamy
+ruda_armada = []
+US_Army = []
+
+#přidání wojáků
+ruda_armada.append(spawn("red"))
+US_Army.append(spawn("blue"))
+
 #pohyb červeného míčku
+
 uhel_micku_cerveneho = math.atan2(y2_cary_sikmo - y1_cary_sikmo, x2_cary_sikmo - x1_cary_sikmo)
 dy = v * math.sin(uhel_micku_cerveneho)
 dx = v * math.cos(uhel_micku_cerveneho)
-pygame.init()
+
 #pohyb modrého míčku
+
 uhel_micku_modreho = math.atan2(x1_cary_sikmo - x2_cary_sikmo, y1_cary_sikmo - y2_cary_sikmo)
-dy1 = v * math.sin(uhel_micku_modreho)
-dx1 = v * math.cos(uhel_micku_modreho)
+dx1 = v * math.sin(uhel_micku_modreho)
+dy1 = v * math.cos(uhel_micku_modreho)
 
 dostrel = 12
 
@@ -59,7 +73,7 @@ pygame.display.set_caption('Střet vojaku')
 okno = pygame.display.set_mode(ROZLISENI_OKNA)
 
 zije = True
-zije = pygame.draw.circle(okno,(255,0,0),(vojak_x, vojak_y), 10), pygame.draw.circle(okno,(0, 150, 255),(vojak_modry_x, vojak_modry_y ), 10)
+#zije = pygame.draw.circle(okno,(255,0,0),(vojak_x, vojak_y), 10), pygame.draw.circle(okno,(0, 150, 255),(vojak_modry_x, vojak_modry_y ), 10)
 while True:
     udalosti = pygame.event.get()
     for u in udalosti:
@@ -74,30 +88,34 @@ while True:
                 pygame.display.iconify()
              
     #pohyb cerveny
-    if zije:
-        vojak_x += dx
-        vojak_y += dy
+    for wojak in ruda_armada:
+        if wojak[1] != "gray":
+            wojak[0][0] += dx
+            wojak[0][1] += dy
 
     #pohyb modry
-    if zije:
-        vojak_modry_x -= dx
-        vojak_modry_y -= dy
+    for wojak in US_Army:
+        if wojak[1] != "gray":
+            wojak[0][0] -= dx
+            wojak[0][1] -= dy
     
     #kolize s vojaky
-    c = ((vojak_x - vojak_modry_x)**2 - (vojak_modry_y - vojak_y)**2) ** (1/2)
-    if c < dostrel:
-        zije = False
-        modra = seda
-        cervena = seda
-        r = 8
+#     c = ((vojak_x - vojak_modry_x)**2 - (vojak_modry_y - vojak_y)**2) ** (1/2)
+#     if c < dostrel:
+#         zije = False
+#         modra = seda
+#         cervena = seda
+#         r = 8
         
     okno.fill(BARVA_POZADI)
     
     pygame.draw.line(okno, (0,0,0), (x1_cary_sikmo, y1_cary_sikmo), (x2_cary_sikmo, y2_cary_sikmo), (tloustka_cary))
     pygame.draw.line(okno,(0,0,0),(x1_cary_rovne,y1_cary_rovne),(x2_cary_rovne,y2_cary_rovne),tloustka_cary) 
     pygame.draw.line(okno,(0,0,0),(x1_cary_dolu,y1_cary_dolu),(x2_cary_dolu,y2_cary_dolu),tloustka_cary)
-    pygame.draw.circle(okno,cervena,(vojak_x, vojak_y), r)
-    pygame.draw.circle(okno,modra,(vojak_modry_x, vojak_modry_y ), r)
+    for wojak in ruda_armada:
+        pygame.draw.circle(okno,wojak[1],(wojak[0][0], wojak[0][1]), r)
+    for wojak in US_Army:
+        pygame.draw.circle(okno,wojak[1],(wojak[0][0], wojak[0][1]), r)
     
     
     pygame.display.update()
